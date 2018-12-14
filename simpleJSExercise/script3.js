7238e29ef32f12ca29837f70c624416806d017d5
Dali Gala <habaresbit@gmail.com>
2018-12-13 23:02:15 -0500

    New excercises for the zenva js course

---
+++ simpleJSExercise/script3.js
@@ -0,0 +1,21 @@
+// Addition
+var a = 1000;
+var b = a + 200 + 100;
+// alert(b);
+// substraction
+var c = 100 - 50;
+// alert(c);
+// multiplication
+var m = c * 10;
+// alert(m);
+// division
+var d = 500 / 2;
+// alert(d);
+var combo = (100 + 50) / 2 * 10;
+// alert(combo);
+// output will be 750 cuz 100 + 50 will go first given the brakets
+// C X 9/5 + 32 = F
+var C = 25;
+var F = C * 9 / 5 + 32;
+alert(F);
+// Simple calculation for farenheit degrees(F) given the celsius degrees(C)
